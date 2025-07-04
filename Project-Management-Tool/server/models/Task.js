9185d6aaed7a5bc7db44eb0abf70a21ede9234f1
Arvind Bhati <145698978+arvindbhati@users.noreply.github.com>
2025-07-04 21:36:15 +0530

    Create Task.js

---
+++ Project-Management-Tool/server/models/Task.js
@@ -0,0 +1,30 @@
+
+
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  status: {
+    type: String,
+    enum: ['todo', 'inprogress', 'done'],
+    default: 'todo',
+  },
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = mongoose.model('Task', taskSchema);
