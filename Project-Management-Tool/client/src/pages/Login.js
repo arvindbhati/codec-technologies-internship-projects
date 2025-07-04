d3ae7333eb7c572966653348cd8f6faafbd8b097
Arvind Bhati <145698978+arvindbhati@users.noreply.github.com>
2025-07-04 21:43:53 +0530

    Create Login.js

---
+++ Project-Management-Tool/client/src/pages/Login.js
@@ -0,0 +1,9 @@
+
+
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const [form, setForm] = useState({ email: '', password: '' });
+  const nav
