661fad234a14e84923d70a254ea76fee4245bdfa
Arvind Bhati <145698978+arvindbhati@users.noreply.github.com>
2025-07-04 21:13:44 +0530

    Update server.js

--- E-commerce-platform/server/server.js
+++ E-commerce-platform/server/server.js
@@ -32,3 +32,6 @@ mongoose.connect(process.env.MONGO_URI, {
 }).catch((err) => {
   console.error('MongoDB connection error:', err);
 });
+
+const checkoutRoute = require('./routes/checkoutRoute');
+app.use('/api/checkout', checkoutRoute);
