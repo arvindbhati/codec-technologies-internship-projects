b590067ea9cbd56305af71ec6492c5346e6321c3
Arvind Bhati <145698978+arvindbhati@users.noreply.github.com>
2025-07-04 20:53:34 +0530

    Create userRoutes.js

---
+++ E-commerce-platform/server/routes/userRoutes.js
@@ -0,0 +1,15 @@
+
+
+const express = require('express');
+const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  getAllUsers
+} = require('../controllers/userController');
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/', getAllUsers); // For admin/testing
+
+module.exports = router;
